adea195cd9f85e829b8bb71de80722b222925f8a
rookie-leo <proleonardopa@gmail.com>
2024-11-22 08:26:49 -0300

    [ API Composition Pattern ]Refatorado metodo getAll

--- src/main/java/com/ead/authuser/specifications/SpecificationTemplate.java
+++ src/main/java/com/ead/authuser/specifications/SpecificationTemplate.java
@@ -1,12 +1,16 @@
 package com.ead.authuser.specifications;
 
+import com.ead.authuser.models.UserCourseModel;
 import com.ead.authuser.models.UserModel;
+import jakarta.persistence.criteria.Join;
 import net.kaczmarzyk.spring.data.jpa.domain.Equal;
 import net.kaczmarzyk.spring.data.jpa.domain.Like;
 import net.kaczmarzyk.spring.data.jpa.domain.LikeIgnoreCase;
 import net.kaczmarzyk.spring.data.jpa.web.annotation.And;
 import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
 import org.springframework.data.jpa.domain.Specification;
+
+import java.util.UUID;
 
 public class SpecificationTemplate {
 
@@ -19,4 +23,11 @@ public class SpecificationTemplate {
     })
     public interface UserSpec extends Specification<UserModel> {}
 
+    public static Specification<UserModel> userCourseId(final UUID courseId) {
+        return ((root, query, criteriaBuilder) -> {
+            query.distinct(true);
+            Join<UserModel, UserCourseModel> userJoin = root.join("usersCourses");
+            return criteriaBuilder.equal(userJoin.get("courseId"), courseId);
+        });
+    }
 }
