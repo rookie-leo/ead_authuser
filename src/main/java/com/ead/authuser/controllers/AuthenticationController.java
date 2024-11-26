fba3820cce88957c535194371b631ae2d1844999
rookie-leo <proleonardopa@gmail.com>
2024-11-26 09:22:00 -0300

    Implementado validação customizada para cadastro de novos usuarios

--- src/main/java/com/ead/authuser/controllers/AuthenticationController.java
+++ src/main/java/com/ead/authuser/controllers/AuthenticationController.java
@@ -2,11 +2,13 @@ package com.ead.authuser.controllers;
 
 import com.ead.authuser.controllers.dtos.UserRecordDto;
 import com.ead.authuser.service.UserService;
+import com.ead.authuser.validations.UserValidator;
 import com.fasterxml.jackson.annotation.JsonView;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,24 +19,24 @@ public class AuthenticationController {
     Logger logger = LogManager.getLogger(AuthenticationController.class);
 
     final UserService userService;
+    final UserValidator userValidator;
 
-    public AuthenticationController(UserService userService) {
+    public AuthenticationController(UserService userService, UserValidator userValidator) {
         this.userService = userService;
+        this.userValidator = userValidator;
     }
 
     @PostMapping("/signup")
     public ResponseEntity<Object> registerUser(
-            @RequestBody @Validated(UserRecordDto.UserView.RegistrationPost.class) @JsonView(UserRecordDto.UserView.RegistrationPost.class) UserRecordDto userRecordDto) {
+            @RequestBody
+            @Validated(UserRecordDto.UserView.RegistrationPost.class)
+            @JsonView(UserRecordDto.UserView.RegistrationPost.class) UserRecordDto userRecordDto,
+            Errors errors
+    ) {
         logger.debug("POST registerUser userRecordDTO receveid: {}", userRecordDto);
-        if (userService.existsByUserName(userRecordDto.userName())) {
-            logger.warn("Username {} is Already Taken!", userRecordDto.userName());
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("Username is Already Taken!");
-        }
-
-        if (userService.existsByEmail(userRecordDto.email())) {
-            logger.warn("Email {} is Already Taken!", userRecordDto.email());
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("Email is Already Taken!");
-        }
+        userValidator.validate(userRecordDto, errors);
+        if (errors.hasErrors())
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors.getAllErrors());
 
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.registerUser(userRecordDto));
     }
