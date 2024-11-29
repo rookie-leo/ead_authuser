66d92515ecbcd4842eb4a5b69eb6ea267e75d665
rookie-leo <proleonardopa@gmail.com>
2024-11-29 09:08:47 -0300

    Implementado metodo para deleção de usuario do sistema e de um curso

--- src/main/java/com/ead/authuser/controllers/UserCourseController.java
+++ src/main/java/com/ead/authuser/controllers/UserCourseController.java
@@ -37,6 +37,7 @@ public class UserCourseController {
             @PageableDefault(sort = "courseId", direction = Sort.Direction.ASC) Pageable pageable,
             @PathVariable(value = "userId") UUID userId
     ) {
+        userService.findById(userId);
         return ResponseEntity.status(HttpStatus.OK).body(courseClient.getAllCoursesByUser(userId, pageable));
     }
 
