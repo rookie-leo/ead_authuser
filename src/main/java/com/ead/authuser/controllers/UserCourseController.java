eda794cab1de36b8641923f8509709bc61d965ca
rookie-leo <proleonardopa@gmail.com>
2025-01-11 12:52:25 -0300

    Implementado access token na comunicação sincrona com o ms-course

--- src/main/java/com/ead/authuser/controllers/UserCourseController.java
+++ src/main/java/com/ead/authuser/controllers/UserCourseController.java
@@ -9,8 +9,10 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
@@ -26,12 +28,14 @@ public class UserCourseController {
         this.userService = userService;
     }
 
+    @PreAuthorize("hasAnyRole('USER')")
     @GetMapping("/users/{userId}/courses")
     public ResponseEntity<Page<CourseRecordDto>> getAllCoursesByUser(
             @PageableDefault(sort = "courseId", direction = Sort.Direction.ASC) Pageable pageable,
-            @PathVariable(value = "userId") UUID userId
+            @PathVariable(value = "userId") UUID userId,
+            @RequestHeader("Authorization") String token
     ) {
         userService.findById(userId);
-        return ResponseEntity.status(HttpStatus.OK).body(courseClient.getAllCoursesByUser(userId, pageable));
+        return ResponseEntity.status(HttpStatus.OK).body(courseClient.getAllCoursesByUser(userId, pageable, token));
     }
 }
